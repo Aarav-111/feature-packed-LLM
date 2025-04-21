6945a42a1b4a284f4cd8bba58e9c1b210b037d3d
Aarav-111 <aaravjaisingh@gmail.com>
2025-04-21 16:05:23 +0530

    connect apps.py

--- Using Gemini API.py
+++
@@ -1,8 +0,0 @@
-from google import genai
-
-client = genai.Client(api_key="YOUR GEMINI API KEY HERE")
-
-response = client.models.generate_content(
-    model="gemini-2.0-flash-lite", contents="huggingface api keys"
-)
-print(response.text)
